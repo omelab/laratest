2d9ade6be86cf9012692928613ebbaec908fd6a2
Abu Bakar <abu.bakar@apsissolutions.com>
2019-12-23 12:53:32 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ajax_upload', 'AjaxUploadController@index');
+
+Route::post('/ajax_upload/action', 'AjaxUploadController@action')->name('ajaxupload.action');
